b53847adc8f204d0a9130a204bbb9baef15faee5
Ewan Jee <830jiforcareer@gmail.com>
2025-04-11 15:29:12 +0900

    authentication 학습

--- spring-security-study/src/main/kotlin/org/springsecuritystudy/configuration/SecurityConfig.kt
+++ spring-security-study/src/main/kotlin/org/springsecuritystudy/configuration/SecurityConfig.kt
@@ -1,20 +1,14 @@
 package org.springsecuritystudy.configuration
 
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.core.Authentication
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @EnableWebSecurity
 @Configuration
@@ -80,6 +74,40 @@ class SecurityConfig {
 //            }
 //        return http.build()
 //    }
+//    @Bean
+//    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+//        http
+//            .authorizeHttpRequests {
+//                it
+//                    .requestMatchers("/login", "/logout", "/logoutSuccess")
+//                    .permitAll()
+//                    .anyRequest()
+//                    .authenticated()
+//            }.formLogin(Customizer.withDefaults())
+//            .logout {
+//                it
+//                    .logoutUrl("/logoutProc")
+//                    .logoutRequestMatcher(AntPathRequestMatcher("/logoutProc", "POST"))
+//                    .logoutSuccessUrl("/login")
+//                    .logoutSuccessHandler(
+//                        LogoutSuccessHandler {
+//                                request: HttpServletRequest,
+//                                response: HttpServletResponse,
+//                                authentication: Authentication?,
+//                            ->
+//                            response.sendRedirect("/logoutSuccess")
+//                        },
+//                    ).deleteCookies("JSESSIONID", "remember-me")
+//                    .invalidateHttpSession(true)
+//                    .clearAuthentication(true)
+//                    .addLogoutHandler { request: HttpServletRequest, response: HttpServletResponse, authentication: Authentication? ->
+//                        request.session?.invalidate()
+//                        SecurityContextHolder.getContextHolderStrategy().context.authentication = null
+//                        SecurityContextHolder.getContextHolderStrategy().clearContext()
+//                    }
+//            }
+//        return http.build()
+//    }
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
@@ -90,39 +118,18 @@ class SecurityConfig {
                     .anyRequest()
                     .authenticated()
             }.formLogin(Customizer.withDefaults())
-            .logout {
-                it
-                    .logoutUrl("/logoutProc")
-                    .logoutRequestMatcher(AntPathRequestMatcher("/logoutProc", "POST"))
-                    .logoutSuccessUrl("/login")
-                    .logoutSuccessHandler(
-                        LogoutSuccessHandler {
-                                request: HttpServletRequest,
-                                response: HttpServletResponse,
-                                authentication: Authentication?,
-                            ->
-                            response.sendRedirect("/logoutSuccess")
-                        },
-                    ).deleteCookies("JSESSIONID", "remember-me")
-                    .invalidateHttpSession(true)
-                    .clearAuthentication(true)
-                    .addLogoutHandler { request: HttpServletRequest, response: HttpServletResponse, authentication: Authentication? ->
-                        request.session?.invalidate()
-                        SecurityContextHolder.getContextHolderStrategy().context.authentication = null
-                        SecurityContextHolder.getContextHolderStrategy().clearContext()
-                    }
-            }
+
         return http.build()
     }
-}
 
-@Bean
-fun inMemoryUserDetailsManager(): InMemoryUserDetailsManager {
-    val user: UserDetails =
-        User
-            .withUsername("user")
-            .password("{noop}1111")
-            .roles("USER")
-            .build()
-    return InMemoryUserDetailsManager(user)
+    @Bean
+    fun inMemoryUserDetailsManager(): InMemoryUserDetailsManager {
+        val user: UserDetails =
+            User
+                .withUsername("user")
+                .password("{noop}1111")
+                .roles("USER")
+                .build()
+        return InMemoryUserDetailsManager(user)
+    }
 }
